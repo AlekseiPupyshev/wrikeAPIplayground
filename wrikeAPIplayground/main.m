44f0aa9db0e9c7d2896b7b9f854454c0886f6c85
Aleksei Pupyshev <alex2-92@mail.ru>
2015-08-02 12:05:32 +0300

    Initial Commit

---
+++ wrikeAPIplayground/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  wrikeAPIplayground
+//
+//  Created by Alexey Pupyshev on 02/08/15.
+//  Copyright (c) 2015 Alexey Pupyshev. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
