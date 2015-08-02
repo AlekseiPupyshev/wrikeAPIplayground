5d6327dddf7a9555b269d085aa11425e3dd77563
Aleksei Pupyshev <alex2-92@mail.ru>
2015-08-02 15:15:41 +0300

    init

---
+++ wrikeAPIplayground/wrikeSDK.m
@@ -0,0 +1,17 @@
+//
+//  wrikeSDK.m
+//  wrikeAPIplayground
+//
+//  Created by Alexey Pupyshev on 02/08/15.
+//  Copyright (c) 2015 Alexey Pupyshev. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+@interface OAuth2Credentials : NSObject
+{
+    NSString *accesToken;
+    NSString *refreshToken;
+    NSString *clientID;
+    NSString *clientSecret;
+}
+@end
