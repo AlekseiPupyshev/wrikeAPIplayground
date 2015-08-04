62410590c00fdad5cbe0066e4698d942365c2b67
Artyom Sheldyayev <cmashinho@gmail.com>
2015-08-04 21:13:53 +0300

    some changes

--- wrikeAPIplayground/wrikeSDK.m
+++ wrikeAPIplayground/wrikeSDK.m
@@ -7,16 +7,150 @@
 //
 
 #import <Foundation/Foundation.h>
-@interface OAuth2Credentials : NSObject
-{
+
+// Модель акаунта
+@interface AccountModel : NSObject {
+    NSString* ID;
+    NSString* name;
+    NSString* rootFolderId;
+    NSString* recycleBinId;
+}
+
+-(void) initWithID: (NSString*) ID
+          withName: (NSString*) name
+  withRootFolderId: (NSString*) rootFolderId
+  withRecycleBinId: (NSString*) recycleBinId;
+
+@end
+@implementation AccountModel
+
+-(void) initWithID: (NSString *) IDM
+          withName: (NSString *) nameM
+  withRootFolderId: (NSString *) rootFolderIdM
+  withRecycleBinId: (NSString *) recycleBinIdM {
+    ID = IDM;
+    name = nameM;
+    rootFolderId = rootFolderIdM;
+    recycleBinId = recycleBinIdM;
+}
+
+@end
+
+// Коллекция аккаунтов
+@interface AccountsCollection : NSObject {
+    NSMutableArray* collection;
+}
+
+-(void) addAccount: (AccountModel*) account;
+-(NSMutableArray*) getAccounts;
+
+@end
+@implementation AccountsCollection
+
+-(void) addAccount: (AccountModel*) account {
+    [collection addObject: account];
+}
+
+-(NSMutableArray*) getAccounts {
+    return collection;
+}
+
+@end
+
+// Коллекция ветки папок
+@interface FolderTreeCollection : NSObject {
+    NSMutableArray* collection;
+}
+
+-(void) addToFolderToTree: (FolderModel*) folder;
+-(NSMutableArray*) getFolderTree;
+
+@end
+@implementation FolderTreeCollection
+
+-(void) addToFolderToTree: (FolderModel*) folder {
+    [collection addObject: folder];
+}
+
+-(NSMutableArray*) getFolderTree {
+    return collection;
+}
+
+@end
+
+// Модель папки
+@interface FolderModel : NSObject {
+    NSString* ID;
+    NSString* title;
+    NSString* description;
+    NSArray* sharedIDs;
+    NSArray* parentIDs;
+    NSString* scope;
+    NSURL* permalink;
+    NSString* workflowID;
+    NSArray* childIDs;
+}
+
+-(void) initFullWithID: (NSString*) IDM
+             withTitle: (NSString*) titleM
+       withDescription: (NSString*) descriptionM
+         withSharedIDs: (NSArray*) sharedIDsM
+         withParentIDs: (NSArray*) parentIDsM
+             withScope: (NSString*) scopeM
+         withPermalink: (NSURL*) permalinkM
+        withWorkflowID: (NSString*) workflowIDM
+          withChildIDs: (NSArray*) childIDsM;
+
+-(void) initForTreeWithID: (NSString*) IDM
+                withTitle: (NSString*) titleM
+             withChildIDs: (NSArray*) childIDsM
+                withScope: (NSString*) scopeM;
+@end
+@implementation FolderModel
+
+-(void) initWithID: (NSString *) IDM
+         withTitle: (NSString *) titleM
+   withDescription: (NSString *) descriptionM
+     withSharedIDs: (NSArray *) sharedIDsM
+     withParentIDs: (NSArray *) parentIDsM
+         withScope: (NSString *) scopeM
+     withPermalink: (NSURL *) permalinkM
+    withWorkflowID: (NSString *) workflowIDM
+      withChildIDs: (NSArray *) childIDsM {
+    ID = IDM;
+    title = titleM;
+    description = descriptionM;
+    [sharedIDs arrayByAddingObjectsFromArray: sharedIDsM];
+    [parentIDs arrayByAddingObjectsFromArray: parentIDsM];
+    scope = scopeM;
+    permalink = permalinkM;
+    workflowID = workflowIDM;
+    childIDs = childIDsM;
+}
+
+@end
+
+// Методы Wrike
+@interface WrikeMethods : NSObject
+
++(AccountsCollection*) getAccounts;
++(FolderTreeCollection*) getFolderTree;
++(FolderModel*) getFolder: (NSString*) ID;
+
+@end
+
+
+@interface OAuth2Credentials : NSObject {
     NSString *accesToken;
     NSString *refreshToken;
     NSString *clientID;
     NSString *clientSecret;
 }
+
 + (void)initWithAccessCode: (NSString *) code withClientID: (NSString *) clientID
                 withSecret: (NSString *) clientSecret;
 + (void)refreshToken;
 @end
+
 @implementation OAuth2Credentials
 @end
