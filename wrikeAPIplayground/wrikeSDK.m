bc386cf022ba66c2c8f8016f3547edc05502aaca
Artyom Sheldyayev <cmashinho@gmail.com>
2015-08-07 23:51:27 +0300

    Added fetchCommentByTaskId

--- wrikeAPIplayground/wrikeSDK.m
+++ wrikeAPIplayground/wrikeSDK.m
@@ -27,13 +27,12 @@ NSString* stringData = @"{ \"kind\": \"task\", \"data\": [ { \"id\": \"IEAAALNZK
             task.Description = [object objectForKey: @"description"];
             task.briefDescription = [object objectForKey: @"briefDescription"];
             // Должен получать массив, получает строку !!!
-            task.parentsIds = [object objectForKey: @"parentsIds"];
+            //task.parentsIds = [object objectForKey: @"parentsIds"];
             // !!!
             task.updatedDate = [object objectForKey: @"createdData"];
             task.createdDate = [object objectForKey: @"updatedData"];
             
             [_items addObject: task];
-            NSLog(@"%@", [task title]);
         }];
     }
 }
@@ -43,6 +42,65 @@ NSString* stringData = @"{ \"kind\": \"task\", \"data\": [ { \"id\": \"IEAAALNZK
  После проверяем JSON на валидность и через блоки/замыкания 
  заполняем наш Task. Потом добовляем в массив
 */
+
+@end
+
+@implementation Task
+
+-(void) sync {
+    // Вначале нужно подрузить дату с _id экземпляра
+    NSData* responseData = [stringData dataUsingEncoding: NSUTF8StringEncoding];
+    NSDictionary* responseDict = [NSJSONSerialization JSONObjectWithData: responseData options: NSJSONReadingMutableContainers error: nil];
+    
+    if([NSJSONSerialization isValidJSONObject: responseDict]) {
+        NSArray* array = [responseDict objectForKey: @"data"];
+        
+        [array enumerateObjectsUsingBlock: ^(NSDictionary* object, NSUInteger idx, BOOL* stop) {
+            __id = [object objectForKey: @"id"];
+            _title = [object objectForKey: @"title"];
+            _Description = [object objectForKey: @"description"];
+            _briefDescription = [object objectForKey: @"briefDescription"];
+            // Должен получать массив, получает строку !!!
+            _parentsIds = [object objectForKey: @"parentsIds"];
+            // !!!
+            _updatedDate = [object objectForKey: @"createdData"];
+            _createdDate = [object objectForKey: @"updatedData"];
+        }];
+    }
+}
+// Происходит тоже самое, что и в TaskCollection(fetch).
+
+@end
+
+@implementation Folder
+/*
++ (instancetype) fetchFolderById: (NSString *) folderID {
+    
+}
+*/
+@end
+
+@implementation CommentsCollection
+
+- (void) fetchCommentsByTaskId: (NSString *) taskId {
+    NSData* responseData = [stringData dataUsingEncoding: NSUTF8StringEncoding];
+    NSDictionary* responseDict = [NSJSONSerialization JSONObjectWithData: responseData options: NSJSONReadingMutableContainers error: nil];
+    
+    if([NSJSONSerialization isValidJSONObject: responseDict]) {
+        NSArray* array = [responseDict objectForKey: @"data"];
+        
+        [array enumerateObjectsUsingBlock: ^(NSDictionary* object, NSUInteger idx, BOOL* stop) {
+            Vomment* comment = [[Vomment alloc] init];
+            comment._id = [object objectForKey: @"id"];
+            comment.authorId = [object objectForKey: @"authorId"];
+            comment.text = [object objectForKey: @"text"];
+            comment.updatedDate = [object objectForKey: @"updatedDate"];
+            comment.taskId = [object objectForKey: @"taskId"];
+            
+            [_items addObject: comment];
+        }];
+    }
+}
 
 @end
 
